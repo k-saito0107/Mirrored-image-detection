f082ce81c6de0ea2e72e96d5dad35c518d03725b
Keisuke Saito <keis0107.ytm@gmail.com>
2020-10-09 21:59:10 +0900

    ver2

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ def train(model, num_epochs,train_loader, test_loader):
     else:
         device = 'cpu'
     print(device)
-    criterion = nn.MSELoss()
+    criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     logs = []
 
